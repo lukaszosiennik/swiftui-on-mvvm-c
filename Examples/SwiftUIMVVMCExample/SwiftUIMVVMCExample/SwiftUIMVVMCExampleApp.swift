a68c45cbd1508c53c416516b75e5de59c99a575b
Łukasz Osiennik <lukasz.osiennik@gmail.com>
2024-07-28 15:16:24 +0200

    Add Example app

---
+++ Examples/SwiftUIMVVMCExample/SwiftUIMVVMCExample/SwiftUIMVVMCExampleApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Łukasz Osiennik on 28/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMCExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
